998730deadda35486977fc9828005863c75d260c
unknown <roman4uni@gmail.com>
2024-06-03 17:54:39 -0300

    djsak

--- acf.m
+++ acf.m
@@ -1,4 +1,4 @@
 function x = acf(f, window, t, lag)
 
-  x = ceil(f(t:t+window) * (f(lag + t: lag + t + window))');
+  x = f(t:t+window) * (f(lag + t: lag + t + window))';
 endfunction
--- main.m
+++ main.m
@@ -14,12 +14,15 @@ t = 0:1/fm: song_lenght - 1/fm;
 
 window_lenght = 1024;
 step = 512;
-window_number = ((song_lenght - window_lenght)/step)+1;
-
+windows_number = ceil(((song_lenght - window_lenght)/step)+1);
+hanning_window = hanning(1024)';
+windows = zeros(window_lenght,windows_number);
 pitch = [];
 db = 0; #debug
-for i=1:window_number
+
+for i=1:windows_number
   display(['window n°: ', num2str(i),'/', num2str(window_number)]);
+  windows(:,i) = song(1+((i-1)*step):(window_lenght+((i-1)*step))).*hanning_window;
   pitch = [pitch yin(song, window_lenght, ((i-1)*step)+ 1, fm, 60, 1000,db)];
   clc;
 endfor
@@ -36,5 +39,40 @@ endfor
 display('Terminado');
 display(['Tiempo transcurrido: ', num2str(toc()), 's']);
 
-##
+##PSOLA ??
+p1nfm = 0;
+p2nfm = 0;
+new_song = zeros(1,2*song_lenght);
+for(i=1:windows_number-2)
+  display(['window n°: ', num2str(i),'/', num2str(window_number)]);
+
+  nfm = (corrected(i)*fm)/pitch(i);
+
+  if(i>2)
+    if(nfm > p2nfm + p1nfm)
+      nfm = p1nfm;
+    endif
+  endif
+
+  p2nfm = p1nfm;
+  p1nfm = nfm;
+
+  paso = (i-1)*step*2;
+  if (pitch(i) == 0)
+    for(j=1:window_lenght)
+      new_song(1+paso+(2*(j-1))) += windows(j,i);
+    endfor
+  else
+    for(j=1:window_lenght)
+    new_song(round(1+paso+(2*(j-1)*(1/(nfm/fm))))) += windows(j,i);
+    endfor
+  endif
+endfor
+
+audiowrite('test.wav',new_song,2*fm);
+
+
+
+
+
 
